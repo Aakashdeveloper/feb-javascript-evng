6a5788ef74c282cc931525434a0667708d7fff19
aakashdeveloper <ahanda205@gmail.com>
2021-02-10 20:45:52 +0530

    A

---
+++ 1DataType.js
@@ -0,0 +1,107 @@
+es
+Ecma Script
+es5
+es6
+es7
+///////////
+String = "fgre","44tf44g5g",'44ffrfr','true',"false"
+Number = 876 7987870 97.897 889 .8978
+Boolean = true/false
+null
+undefined
+
+//es5
+var name = "John"
+var marks = 10
+var pass = true
+undefined
+name
+"John"
+marks
+10
+pass
+true
+
+typeof(name)
+"string"
+typeof(marks)
+"number"
+typeof(pass)
+"boolean"
+var a = 10.66
+undefined
+typeof(a)
+"number"
+
+///Basic Opr
+var a = 10
+var b = 20
+
+a+b
+30 (addition)
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+a%b
+
+var a = 4
+undefined
+var b = 2
+undefined
+a%b
+0
+4%3
+1
+
+2%4 =2
+3%4 =3
+4%4 =0
+
+var a = "hi"
+var b = "javascript"
+a+b  (concat)
+"hijavascript"
+a-b
+NaN (not a number)
+a*b
+NaN
+a/b
+NaN
+a+10
+"hi10"
+a-10
+NaN
+10+a
+"10hi"
+
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+10+"20"+30
+"1020"+30
+"102030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+////////////////
+10+"20"+30-1
+102029
+
+"10"+20+30-1
+102029
+
+10+20+"30"-1
+3029
