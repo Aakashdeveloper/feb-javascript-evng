5bcc297fe2e357caa396bd8f403a577ebf30ce89
aakashdeveloper <ahanda205@gmail.com>
2021-02-12 20:45:56 +0530

    A

---
+++ 2DataTypes.js
@@ -0,0 +1,56 @@
+var a = true
+var b = false
+true is equal to 1
+false is equal to 0
+
+true+true
+2
+
+
+true+true
+2
+true+2
+3
+false*2
+0
+10-true
+9
+"true"-1
+NaN
+"true"-"true"
+NaN
+"a"+true
+"atrue"
+"a"-true
+NaN
+
+var a = "1"
+var b = "2"
+undefined
+a+b
+"12"
+parseInt(a)+parseInt(b)
+3
+parseInt(a)-parseInt(b)
+-1
+var a = "Hi"
+parseInt(a)
+NaN
+
+var a = "1.2"
+parseInt(a)
+1
+parseFloat(a)
+1.2
+
+Number(a)
+1.2
+
+var a ="123Delhi67"
+var b ="Delhi123"
+
+parseInt(a)
+123
+
+parseInt(b)
+NaN
