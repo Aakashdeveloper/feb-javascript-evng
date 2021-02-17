008724732c90fbe5b21d57916ec4029c0fba0fd4
aakashdeveloper <ahanda205@gmail.com>
2021-02-17 20:42:28 +0530

    a

---
+++ 3operations.js
@@ -0,0 +1,59 @@
+= assignment
+== compare the data
+=== compare data and datatype
+
+var a = 10
+var b = '10'
+
+a ==b 
+a===b
+
+var a = 10
+var b = '10'
+
+a==b
+true
+a===b
+false
+
+10<5
+false
+10<=4
+false
+10>=4
+true
+10=>4
+VM276:1 Uncaught SyntaxError: Malformed arrow function parameter list
+10!=5
+true
+10!="10"
+false
+10!=="10"
+true
+
+negation
+var a = true
+!a
+false
+var a = false
+undefined
+!a
+true
+
+var a = 1
+!a
+false
+var a = 0
+undefined
+!a
+true
+
+var a = -1
+!a
+!a
+false
+
+truthy > any number excpet 0, +ve or -ve, any string , true
+falsy > 0, false, null, undefined
+
+all comparistion operator will return boolean
---
+++ 4confirm.js
@@ -0,0 +1,43 @@
+console.log >>>> debug and check output
+alert(); > notification
+confirm() > for yes or no
+prompt() > to take input 
+
+
+alert("Form submitted")
+alert("No Data Found")
+
+confirm("Do You Want To Leave")
+true
+confirm("Do You Want To Leave")
+false
+
+var out = confirm("Do You Want To Leave")
+out
+true
+
+var out = confirm("Do You Want To Leave")
+out
+false
+
+prompt("What is Your Name")
+"Aakash"
+var out = prompt("What is Your Name")
+undefined
+out
+"aakash"
+prompt("What is Your age")
+"442"
+
+var first = prompt("Enter first value")
+var second = prompt("Enter second value")
+var out = first+second
+out
+"1020"
+
+var first = prompt("Enter first value")
+var second = prompt("Enter second value")
+var out = Number(first)+Number(second)
+undefined
+out
+30
